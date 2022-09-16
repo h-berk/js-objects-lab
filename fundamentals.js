181a8a26ffe118a3cc07498ae6ced17ccf97a082
Hasan Berk <hasanberk50@gmail.com>
2022-09-16 15:21:03 +0100

    Arrays.

--- fundamentals.js
+++ fundamentals.js
@@ -1,5 +1,9 @@
 const aCar = {
     owner : 'Joe Bloggs',
+    previous_owners: [
+        {name : 'Pat Smith', address : '1 Main Street'},
+        {name : 'Sheila Dwyer', address : '1 High Street'},
+      ],
     type : {
         make : 'Toyota',
         model : 'Corolla',
@@ -21,5 +25,6 @@ const aCar = {
     }
   } ;
   
-  console.log(aCar.owner + ' drives a ' + aCar.type.make + "." + "The registration of the car is " + aCar.registration.year + "-" + aCar.registration.countyCode + "-" + aCar.registration.number + ".")
-  console.log("It's a " + aCar.colour.exterior + " car, " + aCar.mileage + " mileage, with a " + aCar.colour.interior.shade + " " + aCar.colour.interior.texture + " interior.")
+  console.log(aCar.owner + ' drives a ' + aCar.type.make + "." + "The registration of the car is " + aCar.registration.year + "-" + aCar.registration.countyCode + "-" + aCar.registration.number + ".");
+  console.log("It's a " + aCar.colour.exterior + " car, " + aCar.mileage + " mileage, with a " + aCar.colour.interior.shade + " " + aCar.colour.interior.texture + " interior.");
+  console.log('First owner : ' + aCar.previous_owners[0].name + " residing at " + aCar.previous_owners[0].address );
